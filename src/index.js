d867b26ea1d8f6d7db233ff36710d3fce0ca926d
Artyom Andreev <andreev.artiom@gmail.com>
2021-08-29 20:00:46 +0300

    init: start task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    let matching = {};
+    let openingBrackets = [];
+    let closingBrackets = [];
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        matching[bracketsConfig[i][0]] = bracketsConfig[i][1];
+        openingBrackets.push(bracketsConfig[i][0]);
+        closingBrackets.push(bracketsConfig[i][1]);
+    }
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (openingBrackets.includes(str[i])) {
+            if (closingBrackets.includes(str[i]) && stack.length > 0) {
+                let last = stack.pop();
+                if (str[i] !== last) {
+                    stack.push(last);
+                    stack.push(str[i]);
+                }
+            } else {
+                stack.push(str[i]);
+            }
+        } else {
+            let last = stack.pop();
+            if (str[i] !== matching[last]) {
+                return false;
+            }
+        }
+    }
+
+    if (stack.length !== 0) {
+        return false;
+    }
+
+    return true;
 }
+
